f586e61438dc302f11711d3ce02c626638a61a06
Erick Ortiz <al191362@edu.uaa.mx>
2022-09-11 14:47:55 -0500

    javascript slide still not working

---
+++ slider.js
@@ -0,0 +1,46 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnleft = document.querySelector("#left");
+const btnright = document.querySelector("#right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst =document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+               slider.style.transition ="none";
+               slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+    
+               }, 500);
+
+}
+
+function Prev(){
+    let sliderSection =document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+               slider.style.transition ="none";
+               slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+    
+               }, 500);
+
+}
+
+btnright.addEventListener('click', function(){
+    Next();
+});
+btnleft.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+            Next();
+            },1000);
